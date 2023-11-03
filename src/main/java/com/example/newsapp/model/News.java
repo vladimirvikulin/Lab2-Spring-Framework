07862a0813c0aea74adae470145ff433d7c4f9ac
LLumisS <vasa16671@gmail.com>
2023-11-03 19:13:34 +0200

    base entities added

---
+++ src/main/java/com/example/newsapp/model/News.java
@@ -0,0 +1,51 @@
+package com.example.newsapp.model;
+
+import java.time.LocalDate;
+
+public class News {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDate date;
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
