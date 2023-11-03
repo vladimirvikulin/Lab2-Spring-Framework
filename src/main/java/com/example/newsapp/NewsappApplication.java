7ac6b6fa4eab813dc0d529a6883a02c59c6735d6
vladimirvikulin <v5o4v7a9@gmail.com>
2023-11-03 19:06:58 +0200

    added main application file

---
+++ src/main/java/com/example/newsapp/NewsappApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsappApplication.class, args);
+	}
+
+}
