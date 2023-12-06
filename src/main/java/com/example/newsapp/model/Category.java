4173302ca1785d8a3e4bb9c6cd1ee38715bf044e
LLumisS <vasa16671@gmail.com>
2023-12-06 17:22:26 +0200

    using the built-in jpa repository instead of custom

--- src/main/java/com/example/newsapp/model/Category.java
+++ src/main/java/com/example/newsapp/model/Category.java
@@ -1,16 +1,17 @@
 package com.example.newsapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
+    @org.springframework.data.annotation.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -18,5 +19,13 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/main/java/com/example/newsapp/model/News.java
+++ src/main/java/com/example/newsapp/model/News.java
@@ -1,21 +1,26 @@
 package com.example.newsapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDate;
 
+@Entity
 public class News {
+
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
     private LocalDate date;
+
+    @ManyToOne
     private Category category;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getTitle() {
         return title;
@@ -47,5 +52,13 @@ public class News {
 
     public void setCategory(Category category) {
         this.category = category;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
