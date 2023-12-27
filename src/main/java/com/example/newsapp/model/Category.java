6c961ed208f966cd50aaed89f43f2c8aa390535b
LLumisS <vasa16671@gmail.com>
2023-12-27 18:29:47 +0200

    category dao implementation using jdbc template

--- src/main/java/com/example/newsapp/model/Category.java
+++ src/main/java/com/example/newsapp/model/Category.java
@@ -1,30 +1,11 @@
 package com.example.newsapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
+@Getter
+@Setter
 public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
