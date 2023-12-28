687356593c7f9da119114957e919d573067e7625
LLumisS <vasa16671@gmail.com>
2023-12-28 18:09:05 +0200

    added: news entity

--- src/main/java/com/example/newsapp/model/News.java
+++ src/main/java/com/example/newsapp/model/News.java
@@ -1,5 +1,6 @@
 package com.example.newsapp.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +8,15 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
 public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
     private LocalDate date;
+
+    @ManyToOne
     private Category category;
 }
