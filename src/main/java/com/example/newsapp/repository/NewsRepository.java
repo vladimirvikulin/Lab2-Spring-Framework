5969ed391d2875c2679142c049738ff733f195dc
danil2205 <04danil@gmail.com>
2023-12-28 18:42:41 +0200

    added: news repository based on CrudRepository

--- src/main/java/com/example/newsapp/repository/NewsRepository.java
+++ src/main/java/com/example/newsapp/repository/NewsRepository.java
@@ -1,113 +1,30 @@
 package com.example.newsapp.repository;
 
-import com.example.newsapp.model.Category;
 import com.example.newsapp.model.News;
-import lombok.AllArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-@AllArgsConstructor
-public class NewsRepository implements NewsDAO {
-    private final JdbcTemplate jdbcTemplate;
-    private final CategoryRepository categoryDAO;
+public interface NewsRepository extends CrudRepository<News, Long> {
+    @Query("SELECT n FROM News n WHERE n.category.id = :id")
+    List<News> findByCategory(@Param("id") Long categoryId);
 
-    private static class NewsRowMapper implements RowMapper<News> {
-        CategoryRepository categoryDAO;
+    List<News> findByTitleContaining(String keyword);
 
-        public NewsRowMapper(CategoryRepository categoryDAO) {
-            this.categoryDAO = categoryDAO;
-        }
-        @Override
-        public News mapRow(ResultSet rs, int rowNum) throws SQLException {
-            News news = new News();
-            news.setId(rs.getLong("id"));
-            news.setTitle(rs.getString("title"));
-            news.setContent(rs.getString("content"));
-            news.setDate(rs.getDate("date").toLocalDate());
-
-            Optional<Category> category = categoryDAO.findById(rs.getLong("category_id"));
-            category.ifPresent(news::setCategory);
-
-            return news;
-        }
-    }
-
-    @Override
-    public List<News> findAll() {
-        String sql = "SELECT * FROM news";
-        return jdbcTemplate.query(sql, new NewsRowMapper(categoryDAO));
-    }
-
-    @Override
-    public Optional<News> findById(Long id) {
-        String sql = "SELECT * FROM news WHERE id = ?";
-        List<News> result = jdbcTemplate.query(sql, new NewsRowMapper(categoryDAO), id);
-        return result.isEmpty() ? Optional.empty() : Optional.of(result.get(0));
-    }
-
-    @Override
-    public List<News> findByCategory(Category category) {
-        String sql = "SELECT * FROM news WHERE category_id = ?";
-        return jdbcTemplate.query(sql, new NewsRowMapper(categoryDAO), category.getId());
-    }
-
-    @Override
-    public List<News> findByTitleContaining(String keyword) {
-        String sql = "SELECT * FROM news WHERE title LIKE ?";
-        return jdbcTemplate.query(sql, new NewsRowMapper(categoryDAO), "%" + keyword + "%");
-    }
-
-    @Override
-    public Long save(News news) {
-        String sql = "INSERT INTO news(title, content, date, category_id) " +
-                "VALUES (?, ?, ?, ?) RETURNING id";
-        return jdbcTemplate.queryForObject(
-                sql, Long.class,
-                news.getTitle(),
-                news.getContent(),
-                news.getDate(),
-                news.getCategory().getId()
-        );
-    }
-
-    @Override
-    public boolean update(News news) {
-        String sql = "UPDATE news SET title = ?, content = ?, date = ?, category_id = ? WHERE id = ?";
-
-        int rowsUpdated = jdbcTemplate.update(
-                sql,
-                news.getTitle(),
-                news.getContent(),
-                news.getDate(),
-                news.getCategory().getId(),
-                news.getId()
-        );
-
-        return rowsUpdated > 0;
-    }
-
-    @Override
-    public boolean deleteAll(List<News> news) {
-        String sql = "DELETE FROM news WHERE id IN (?)";
-        Object[] ids = news.stream().map(News::getId).toArray();
-        if (ids.length == 0) {
-            return true;
-        }
-        int rowsUpdated = jdbcTemplate.update(sql, ids);
-        return rowsUpdated > 0;
-    }
-
-    @Override
-    public boolean delete(News news) {
-        String sql = "DELETE FROM news WHERE id = ?";
-        int rowsUpdated = jdbcTemplate.update(sql, news.getId());
-        return rowsUpdated > 0;
-    }
+    @Modifying
+    @Query("UPDATE News n SET n.title = :newTitle, n.content = :newContent, n.date = :newDate, n.category.id = :newCategoryId WHERE n.id = :id")
+    int update(
+            @Param("newTitle") String newTitle,
+            @Param("newContent") String newContent,
+            @Param("newDate") LocalDate newDate,
+            @Param("newCategoryId") Long newCategoryId,
+            @Param("id") Long id
+    );
 }
