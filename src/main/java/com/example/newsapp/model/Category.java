24d8aeea2862bc51d27514e82aa5303be34c44a9
vladimirvikulin <v5o4v7a9@gmail.com>
2023-12-28 18:13:20 +0200

    added Category entity

--- src/main/java/com/example/newsapp/model/Category.java
+++ src/main/java/com/example/newsapp/model/Category.java
@@ -1,11 +1,16 @@
 package com.example.newsapp.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@NamedQuery(name = "Category.findByName", query = "SELECT c FROM Category c WHERE c.name = :name")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
